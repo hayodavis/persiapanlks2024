98fb971064d769fd5642c6921632ee943cd84a86
Firda Rahayu <firdarahayu105@gmail.com>
2024-01-14 11:58:32 +0700

    Firda mengedit bagian D1. PHP Calendar

--- MODUL - SPEEDTEST/Firda/D1. PHP Calendar/index.php
+++ MODUL - SPEEDTEST/Firda/D1. PHP Calendar/index.php
@@ -32,21 +32,41 @@ if (isset($_GET['next'])) {
     }
 
     .calendar-container {
-      border-top: 5px solid #fc0303;
-      width: 28%;
-      padding-top: 10px;
-      padding-bottom: 15px;
       text-align: center;
-      border-right: 2px solid #c7c7c7;
-      border-left: 2px solid #c7c7c7;
-      border-bottom: 3px solid #c7c7c7;
+      border-top: 5px solid #F0677D;
+      border-right: 1px solid #c7c7c7;
+      border-left: 1px solid #c7c7c7;
+      border-bottom: 1px solid #c7c7c7;
+      padding: 25px;
+    }
+
+    .top-row {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      background-color: #fff;
+      border-bottom: 1px solid #c7c7c7;
+      padding-bottom: 15px;
+    }
+
+    .arrow-btn {
+      cursor: pointer;
+    }
+
+    .month-year {
+      color: #2a343d;
+      font-size: 18px;
+    }
+
+    .year {
+      color: #2a343d;
+      font-size: 14px;
     }
 
     #calendar {
       border-collapse: collapse;
       margin-top: 20px;
-      width: 300px;
-      margin: 0 auto;
+      width: 100%;
       text-align: center;
     }
 
@@ -58,44 +78,15 @@ if (isset($_GET['next'])) {
 
     #calendar th {
       background-color: #fff;
-    }
-
-    #calendar th:nth-child(1) {
-      color: #fc0303;
+      color: #F0677D;
     }
 
     .highlight-today {
-      background-color: #fc0303;
+      background-color: #F0677D;
       color: white;
       font-weight: bold;
     }
 
-    .btn-container {
-      margin-top: 20px;
-      display: flex;
-      align-items: center;
-    }
-
-    .arrow-btn {
-      padding: 10px;
-      font-size: 16px;
-      cursor: pointer;
-    }
-
-    .arrow-btn:hover {
-      background-color: #fc0303;
-      color: white;
-    }
-
-    .month-year {
-      color: #000;
-      font-size: 18px;
-      margin-bottom: 10px;
-    }
-
-    th {
-      color: #fc0303;
-    }
   </style>
 </head>
 
@@ -103,8 +94,17 @@ if (isset($_GET['next'])) {
 
   <div class="calendar-container">
 
-    <div class="month-year">
-      <?php echo date('F Y', mktime(0, 0, 0, $currentMonth, 1, $currentYear)); ?>
+    <div class="top-row">
+      <a href="?prev" class="arrow-btn"><img src="left.png" alt="prev"></a>
+      <div class="month-year">
+        <?php echo date('F', mktime(0, 0, 0, $currentMonth, 1, $currentYear)); ?>
+        <br>
+        <div class="year">
+        <?php echo date('Y', mktime(0, 0, 0, $currentMonth, 1, $currentYear)); ?>
+      </div>
+      </div>
+      
+      <a href="?next" class="arrow-btn"><img src="right.png" alt="next"></a>
     </div>
 
     <table id="calendar">
@@ -149,11 +149,6 @@ if (isset($_GET['next'])) {
         ?>
       </tbody>
     </table>
-  </div>
-
-  <div class="btn-container">
-    <a href="?prev" class="arrow-btn">&lt;&lt; Prev</a>
-    <a href="?next" class="arrow-btn">Next &gt;&gt;</a>
   </div>
 
 </body>
